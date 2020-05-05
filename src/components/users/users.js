c75fd21562ec33967569bc95dfe4ba4521f56fad
Lucas Manzanelli <lucas.manzanelli@ing.austral.edu.ar>
2020-05-05 11:05:01 -0300

    Renderer users. Bug with undefined values get from api by post.

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,67 +1,102 @@
 import {UserService} from "../../shared/services/user.service.js";
-import {UserModel} from "../../shared/models/user.model.js";
 
-//INTERNAL FUNCTIONS
-const icon = (fasItem) => {
-    const icon = document.createElement('i');
-    icon.classList.add('fas');
-    icon.classList.add(fasItem);
-    return icon;
-};
 
-const renderUserRow = (simpleUserModel) => {
-    const tbody = document.getElementById('usersTable')
-        .getElementsByTagName('tbody')[0];
+class Users {
+    userService = new UserService();
 
-    const row = tbody.insertRow();
-    row.insertCell().appendChild(document.createTextNode(simpleUserModel.firstName));
-    row.insertCell().appendChild(document.createTextNode(simpleUserModel.lastName));
-    row.insertCell().appendChild(document.createTextNode(simpleUserModel.mail));
-    row.insertCell().appendChild(document.createTextNode(simpleUserModel.phone));
+    constructor() {
+        this.getAllUsers();
+    }
 
-    row.insertCell().appendChild(icon('fa-calendar-alt'));
-    row.insertCell().appendChild(icon('fa-edit'));
-    row.insertCell().appendChild(icon('fa-trash'));
+    //Renderer DOM Methods
 
-};
+    icon(fasItem) {
+        const icon = document.createElement('i');
+        icon.classList.add('fas');
+        icon.classList.add(fasItem);
+        return icon;
+    };
 
-const renderUsersLoading = () => {
-    const section = document.getElementById('users');
+    renderUserRow(simpleUserModel) {
+        const tbody = document.getElementById('usersTable')
+            .getElementsByTagName('tbody')[0];
 
-    const loader = document.createElement('div');
-    loader.className = 'loader';
+        const row = tbody.insertRow();
+        row.insertCell().appendChild(document.createTextNode(simpleUserModel.firstName));
+        row.insertCell().appendChild(document.createTextNode(simpleUserModel.lastName));
+        row.insertCell().appendChild(document.createTextNode(simpleUserModel.mail));
+        row.insertCell().appendChild(document.createTextNode(simpleUserModel.phone));
 
-    const loadContainer = document.createElement('div');
-    loadContainer.className = 'loader-container';
-    loadContainer.append(loader);
+        row.insertCell().appendChild(this.icon('fa-calendar-alt'));
+        row.insertCell().appendChild(this.icon('fa-edit'));
+        row.insertCell().appendChild(this.icon('fa-trash'))
+            .addEventListener("click", () => {
+                this.deleteUser(simpleUserModel)
+            });
+    };
 
-    section.insertAdjacentElement('beforeend', loadContainer);
-};
+    renderLoading() {
+        const section = document.getElementById('users');
 
-const renderNoneUsers = () => {
-    const section = document.getElementById('users');
+        const loader = document.createElement('div');
+        loader.className = 'loader';
 
-    const div = document.createElement('div');
-    div.className = 'loader-container';
+        const loadContainer = document.createElement('div');
+        loadContainer.className = 'loader-container';
+        loadContainer.append(loader);
 
-    const span = document.createElement('span');
-    span.appendChild(document.createTextNode('None Users found!'));
+        section.insertAdjacentElement('beforeend', loadContainer);
+    };
 
-    div.appendChild(span);
+    renderNoneUsers() {
+        const section = document.getElementById('users');
 
-    section.insertAdjacentElement('beforeend', div);
-};
+        const div = document.createElement('div');
+        div.className = 'loader-container';
 
-const destroyElement = (elementClass) => {
-    const element = document.querySelector(elementClass);
-    if (element) element.parentNode.removeChild(element);
-};
+        const span = document.createElement('span');
+        span.appendChild(document.createTextNode('None Users found!'));
 
-//EXECUTION
-renderUsersLoading();
+        div.appendChild(span);
+
+        section.insertAdjacentElement('beforeend', div);
+    };
+
+    destroyElement(elementClass) {
+        const element = document.querySelector(elementClass);
+        if (element) element.parentNode.removeChild(element);
+    };
+
+    //Internal methods
+
+    getAllUsers() {
+        this.renderLoading();
+        this.userService.requestUsers().then(users => {
+            if (users.length === 0)
+                this.renderNoneUsers();
+            else {
+                users.forEach(u => this.renderUserRow(u));
+                this.destroyElement('loader-container');
+            }
+            this.destroyElement('.loader-container');
+        });
+    }
+
+    deleteUser(id) {
+        this.userService.deleteUser(id)
+            .then(() => this.getAllUsers())
+    }
+
+    submitUser(userModel) {
+        this.userService.postUser(userModel)
+            .then(() => this.getAllUsers())
+    }
+
+}
+
+new Users();
 
 const userService = new UserService();
-
 // userService.postUser(new UserModel(
 //     'Lucas',
 //     'Manzanelli',
@@ -76,13 +111,4 @@ const userService = new UserService();
 // ))
 //     .then(res => console.log(res))
 
-userService.requestUsers().then(users => {
-    if (users.length === 0)
-        renderNoneUsers();
-    else {
-        users.forEach(u => renderUserRow(u));
-        destroyElement('loader-container');
-    }
-    destroyElement('.loader-container');
-});
 
--- src/shared/services/user.service.js
+++ src/shared/services/user.service.js
@@ -2,8 +2,8 @@ import {SimpleUserModel} from "../models/simple-user.model.js";
 
 const url = 'http://dpoi2012api.appspot.com/api/1.0';
 
-const parseUser = json => {
-    console.log('JSON RESPONSE', json.id);
+const parseJSON = json => {
+    console.log('JSON RESPONSE', json);
     return new SimpleUserModel(
         json.firstName,
         json.lastName,
@@ -19,15 +19,62 @@ class UserService {
 
     async requestUsers() {
         return new Promise((resolve, reject) => {
-            fetch(url + '/list_delay?credential=dpoi').then(response => {
+            fetch(url + '/list?credential=lmanzanelli').then(response => {
                 if (response.ok)
                     resolve(response.json()
-                        .then(data => data.payload.items.map(parseUser)));
+                        .then(data =>
+                            data.payload.items.map(parseJSON)
+                        ));
                 else reject(response.error())
             })
         });
     }
 
+    async postUser(userModel) {
+        //todo FormData doesnt work for some reason
+        // reference -> https://desarrolloweb.com/articulos/fetch-post-ajax-javascript.html
+        const data = new URLSearchParams();
+
+        //Objects entries return an array of string tuples as (key,value)
+        Object.entries(userModel)
+            .forEach(([key, value]) => {
+                data.append(key, '' + value);
+            });
+
+        return new Promise((resolve, reject) => {
+            fetch(url + '/create?credential=lmanzanelli',
+                {
+                    method: 'POST',
+                    body: JSON.stringify(Object.entries(userModel)),
+                }
+            )
+                .then(response => {
+                    if (response.ok) {
+                        resolve(userModel);
+                    } else reject(response.error())
+                })
+        });
+    }
+
+    async deleteUser(userId) {
+        const data = new URLSearchParams();
+        data.append('id', userId);
+
+        new Promise((resolve, reject) => {
+            fetch(url + '/delete?credential=lmanzanelli',
+                {
+                    method: 'POST',
+                    body: data,
+                }
+            )
+                .then(response => {
+                    if (response.ok) {
+                        resolve(response.json().then(data => data));
+                    } else reject(response.error())
+                })
+        });
+
+    }
 }
 
 export {UserService}
